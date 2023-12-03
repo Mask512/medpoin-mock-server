0ab9bb475fde8c21d83f14a3739b0206eb7bb7b8
Mask512 <Mask.roktz@gmail.com>
2023-12-04 02:43:18 +0700

    add icd

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const pasien = require('./data/pasien.json');
 const dokter = require('./data/dokter.json');
 const perawat = require('./data/perawat.json');
 const admin = require('./data/admin.json');
+const icd9 = require('./data/icd9.json');
+const icd10 = require('./data/icd10.json');
 
 const routes = [
   {
@@ -38,6 +40,20 @@ const routes = [
     path: '/admin',
     handler: (request, h) => {
         return admin;
+    },
+  },
+  {
+    method: 'GET',
+    path: '/icd9',
+    handler: (request, h) => {
+        return icd9;
+    },
+  },
+  {
+    method: 'GET',
+    path: '/icd10',
+    handler: (request, h) => {
+        return icd10;
     },
   },
 ];
